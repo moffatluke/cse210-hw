83a15c9632c9644e316890b5fe0eaaaca122f1c1
moffatluke <lukebmoffat@gmail.com>
2026-01-08 20:07:26 -0700

    prep1 and prep2 are done

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,29 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.Write("Enter score ");
+        string userAnswer = Console.ReadLine();
+        int score = int.Parse(userAnswer);
+
+        if (score >= 90)
+        {
+            Console.WriteLine("A");
+        }
+        else if (score >= 80)
+        {
+            Console.WriteLine("B");
+        }
+        else if (score >= 70)
+        {
+            Console.WriteLine("C");
+        }
+        else if (score >= 60)
+        {
+            Console.WriteLine("D");
+        }
+        else
+        {
+            Console.WriteLine("F");
+        }
     }
 }
