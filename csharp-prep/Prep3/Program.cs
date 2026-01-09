ea29716c2d6e9f0c38a46b2c366e391b6081022a
moffatluke <lukebmoffat@gmail.com>
2026-01-09 12:20:14 -0700

    prep 3 and 4 are done.

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,47 @@
 using System;
+using System.ComponentModel;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        /*These comments are for me to think through this
+        Basicly this is the starting block that
+        makes the number and saves it as a varaible
+        */
+        Random numberGenerator = new Random();
+        int magicNumber = numberGenerator.Next(1, 101);
+
+
+
+        /*
+        Now I need the to create the loop 
+        - prompts the user for a guess
+        - checks it agains the set magic number
+        - compares the values and writes a line to the user
+        - then asks the user to guess again
+        - if the number is correct it displays a congrats message
+        */
+        //declare the guess variable and set it 
+        int guess = -1;
+
+        do
+        { 
+
+            Console.Write("Guess the Magic Number? ");
+            guess = int.Parse(Console.ReadLine()); 
+
+            if (guess >= magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else if (guess <= magicNumber)
+            {
+            Console.WriteLine("Higher");
+            }
+            
+        }
+        while (guess != magicNumber);
+        Console.WriteLine("You guessed it!");
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,58 @@
-using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        Console.WriteLine("Enter a list of whole numbers, type 0 when finished.");
+        //create list to add values to
+        List<int> numbers = new List<int>();
+        int userNumber = -1;
+        do
+        {
+            Console.Write("Enter a Number ");
+            userNumber = int.Parse(Console.ReadLine());
+            //add the number form the user to the list. 
+            numbers.Add(userNumber);
+        } while (userNumber != 0);
+
+        //to check to see if the numbers were stored in the list properly
+        /*for (int i = 0; i < numbers.Count; i++)
+        {
+            Console.WriteLine(numbers[i]);
+        }*/
+
+
+
+        //Math time!
+        //We should add all values to get sum
+
+        int sum = 0;
+        foreach (int num in numbers)
+        {
+            sum += num;
+        }
+
+        //average time have to make it  float to not a true average
+        //casting to make sure it is a float but we can still use the int version of sum later
+        float average = ((float)sum) / numbers.Count;
+
+        //Max time
+        //declare varaible
+        int max = numbers[0];
+
+        foreach(int num in numbers)
+        {
+            if (num > max)
+            {
+                max = num;
+            }
+        }
+        //print statements
+        Console.WriteLine($"The sum is: {sum}");
+        Console.WriteLine($"The average is: {average}");
+        Console.WriteLine($"The max is: {max}");
     }
+
 }
