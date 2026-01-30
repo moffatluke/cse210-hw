e195e8738f676449c10083e29f4af5999eed2b9a
moffatluke <lukebmoffat@gmail.com>
2026-01-29 17:38:34 -0700

    added some more print statements and changed the for-loop output to look better

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,8 @@ class Program
 {
     static void Main(string[] args)
     {
+
+        Console.WriteLine("Samples go here:");
         //here is where we can call the fraction class
         //make methods lets do 4 
         Fraction f1  = new Fraction();
@@ -24,15 +26,17 @@ class Program
         Console.WriteLine(f3.GetFractionString());
         Console.WriteLine(f3.GetDecimalValue());
 
+        Console.WriteLine("--------------------------------------------------------");
 
-        //loop time
+
+        //Loop time
         //need to get random numbers for the fractions
         Random random = new Random();
 
         //fraction #5 object
         Fraction f5 = new Fraction();
 
-        //for loop to loop through 20 lines
+        //for-loop to loop through 20 random lines as requested
         for (int i =0; i<20; i++)
         {
             int topNum = random.Next(1, 11);
@@ -41,10 +45,11 @@ class Program
             f5.SetTop(topNum);
             f5.Setbottom(bottomNum);
             //now display with loop counter for sanity
-            Console.WriteLine ($"Fraction {i +1};L: ");
-            Console.WriteLine($"String: {f5.GetFractionString()}");
+            Console.Write($"Fraction {i +1} | ");
+            Console.Write($"String: {f5.GetFractionString()}");
             Console.WriteLine($"Decimal: {f5.GetDecimalValue()}");
-            Console.WriteLine("There you go!!!!");
         }
+
+        Console.WriteLine("It worked!!");
     }
 }
