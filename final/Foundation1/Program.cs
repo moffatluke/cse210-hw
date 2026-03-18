833711c3bdd32c3f58cc950c309cf53e15057cc8
moffatluke <lukebmoffat@gmail.com>
2026-03-18 16:10:19 -0600

    Finished Foundation 1: YOUTUBE.

--- final/Foundation1/Program.cs
+++ final/Foundation1/Program.cs
@@ -5,15 +5,44 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Foundation1 World!");
+        Console.Clear();
+        Console.WriteLine("VIDEOS:");
+        Console.WriteLine();
+
 
         List<Video> videos = new List<Video>();
 
+        //Video 1 coding 
         Video v1 = new Video("Clean code", "Uncle Bob", 3600);
-        v1.AddComment(new Comments("Allice", "Great video!!"));
-        v1.AddComment(new Comments("Bob", "Very helpful!!"));
+        v1.AddComment(new Comment("Allice", "Great video!!"));
+        v1.AddComment(new Comment("Bob", "Very helpful!!"));
         videos.Add(v1);
 
+
+
+        //Video 2 spider-man
+        Video v2 = new Video("Spider-Man: Brand New Day Trailer", "Marvel Entertainment", 2000);
+        v2.AddComment(new Comment("Steven", "I's so excited for this!!"));
+        v2.AddComment(new Comment("Roger", "This is the best thing to happen this year!!"));
+        videos.Add(v2); //add everything to the v2 video
+
+        //video 3 dune
+        Video v3 = new Video("Dune: Part Two Trailer", "Warner Bros", 2880);
+        v3.AddComment(new Comment("Marcus", "The cinematography is insane!"));
+        v3.AddComment(new Comment("Sarah", "Can't wait for this movie."));
+        v3.AddComment(new Comment("Jordan", "Best sci-fi movie ever made."));
+        videos.Add(v3);
+
+        //Video 3 apple
+        Video v4 = new Video("iPhone 15 Pro Max Review", "Apple", 4200);
+        v4.AddComment(new Comment("Lisa", "The camera is absolutely incredible."));
+        v4.AddComment(new Comment("Chris", "Worth every penny."));
+        v4.AddComment(new Comment("Maya", "Game-changing technology."));
+        videos.Add(v4);
+
+
+
+        //Video 3
         foreach (Video video in videos)
         {
             video.Display();
--- final/Foundation1/Video.cs
+++ final/Foundation1/Video.cs
@@ -4,7 +4,7 @@ public class Video
     private string _title;
     private string _author;
     private int _lengthInSeconds;
-    private List<Comment> _commments;
+    private List<Comment> _comments;
 
     //Constructor
     public Video(string title, string author, int lengthInSeconds)
@@ -13,12 +13,34 @@ public class Video
         _author = author;
         _lengthInSeconds = lengthInSeconds;
 
-        List<Comment> comment = new List<Comment>();
+        _comments = new List<Comment>();
     }
+
+
+// Methods
+
 
     public void AddComment(Comment comment)
     {
-        
+        _comments.Add(comment);
     }
     
+    public int GetNumberOfComments()
+    {
+        return _comments.Count;
+    }
+public void Display()
+{
+    Console.WriteLine($"Title: {_title}");
+    Console.WriteLine($"Author: {_author}");
+    Console.WriteLine($"Length: {_lengthInSeconds} seconds");
+    Console.WriteLine($"Number of comments: {GetNumberOfComments()}");
+
+    foreach (Comment c in _comments)
+    {
+        Console.WriteLine($"  {c.GetCommenterName()}: {c.GetCommentText()}"); 
+        // This gets the name and text that we need to put on the video
+    }
+}
+
 }
