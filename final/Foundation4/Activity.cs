7d6810b0d93fdb3437225301b6e67bde50d0d573
moffatluke <lukebmoffat@gmail.com>
2026-03-25 09:36:52 -0600

    Added some more documentation for the super class.

--- final/Foundation4/Activity.cs
+++ final/Foundation4/Activity.cs
@@ -33,7 +33,12 @@ public abstract class Activity
     {
         return $"{GetDate()} {GetType().Name} ({GetLengthMinutes()} min) - Distance: {GetDistance():0.00} miles, Speed: {GetSpeed():0.00} mph, Pace: {GetPace():0.00} min per mile";
     }
+
+    //GetType().Name is used to return the name of the class at runtime
+    //GetType is a built in function
+    //.Name is built-in aswell. It returns the name as a string.
+    //GetType() is method called on a object
+    //.Name is a property on what GetType() returns.
     // Used :0.00 after some of the methods to format them. It round them and only displays the two decimal palces. 
     //learned on https://learn.microsoft.com/en-us/dotnet/standard/base-types/standard-numeric-format-strings
-    //Used GetType().Name to get the name of the the activity. 
 }
